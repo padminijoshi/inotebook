b34aff55df09e056221d4dd6d017450fdcaaaa09
padminijoshi <joshipj2000@gmail.com>
2023-12-11 20:09:17 +0530

    8th

--- backend/routes/auth.js
+++ backend/routes/auth.js
@@ -42,7 +42,46 @@ body('password','password must be atleast 5 characters').isLength({min:5})], asy
   
   }catch(error){
     console.error(error.message);
-    res.status(500).send("Some Error occured");
+    res.status(500).send("Interval server Error");
   }
 })
+
+//create a user using: POST "/api/auth/login". no login required
+router.post('/login',[
+  body('email','Enter a valid email').isEmail(),
+  body('password','Password cannot be blank').exists()
+  ],async(req,res)=>{
+
+    //if there are errors, return Bad request and the errors
+   const errors = validationResult(req);
+   if (!errors.isEmpty()) {
+     return res.status(400).json({erros:errors.array()});
+   }
+
+   const{email,password} = req.body;
+   try {
+    let user = await User.findOne({email});
+    if(!user){
+      return res.status(400).json({error:"Please try to login with correct credentials"});
+    }
+    const passwordCompare = await bcrypt.compare(password,user.password);
+    if(!passwordCompare){
+      return res.status(400).json({error:"Please try to login with correct credentials"});
+    }
+    const data = {
+      user:{
+        id: user.id
+      }
+    }
+    const authtoken = jwt.sign(data,JWT_SECRET);
+    res.json({authtoken})
+   } catch (error) {
+    console.error(error.message);
+    res.status(500).send("Interval server Error");
+   }
+
+
+
+})
+
 module.exports = router
