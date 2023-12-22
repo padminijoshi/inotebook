fa4ecbccf18fd952e284cdfe16b727fa786c75c1
padminijoshi <joshipj2000@gmail.com>
2023-12-22 20:23:16 +0530

    i10

--- src/context/notes/NoteState.js
+++ src/context/notes/NoteState.js
@@ -15,8 +15,8 @@ const NoteState = (props) => {
             "auth-token": "eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJ1c2VyIjp7ImlkIjoiNjU3NzE2Y2M0MzIzODM4MzNlMGYxYzcyIn0sImlhdCI6MTcwMjMwNzkzNX0.0FPl3L3O7piXR5cpK3L5v_EHEo0d-04U9joC8gIzeQk"
           }
         });
-        const json = await response.json()
-        console.log(json)
+        const json = await response.json();
+        console.log(json);
         setNotes(json)
       }
       
@@ -32,7 +32,8 @@ const NoteState = (props) => {
          
           body: JSON.stringify({title,description,tag}), 
         });
-
+        const json = await response.json();
+        console.log(json);
         console.log("Adding a new note")
         const note = {
           "_id": "657855e693e673d141c4c688",
@@ -56,7 +57,7 @@ const NoteState = (props) => {
             "auth-token": "eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJ1c2VyIjp7ImlkIjoiNjU3NzE2Y2M0MzIzODM4MzNlMGYxYzcyIn0sImlhdCI6MTcwMjMwNzkzNX0.0FPl3L3O7piXR5cpK3L5v_EHEo0d-04U9joC8gIzeQk"
           }
         });
-        const json = response.json();
+        const json = await response.json();
         console.log(json)
         console.log("Deleting the note with id" + id);
         const newNotes = notes.filter((note)=>{return note._id!==id})
@@ -66,26 +67,28 @@ const NoteState = (props) => {
       const editNote = async(id, title, description, tag)=>{
         //api call
         const response = await fetch(`${host}/api/notes/updatenote/${id}`, {
-          method: "POST", 
-          headers: {
-            "Content-Type": "application/json",
-            "auth-token": "eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJ1c2VyIjp7ImlkIjoiNjU3NzE2Y2M0MzIzODM4MzNlMGYxYzcyIn0sImlhdCI6MTcwMjMwNzkzNX0.0FPl3L3O7piXR5cpK3L5v_EHEo0d-04U9joC8gIzeQk"
-          },
-         
-          body: JSON.stringify(title,description,tag), 
+          method: 'PUT',
+        headers: {
+        'Content-Type': 'application/json',
+        "auth-token": "eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJ1c2VyIjp7ImlkIjoiNjU3NzE2Y2M0MzIzODM4MzNlMGYxYzcyIn0sImlhdCI6MTcwMjMwNzkzNX0.0FPl3L3O7piXR5cpK3L5v_EHEo0d-04U9joC8gIzeQk"
+        },
+          body: JSON.stringify({title, description, tag})
         });
-        const json = response.json();
+        const json = await response.json();
+        console.log(json);
+        let newNotes = JSON.parse(JSON.stringify(notes))
         //logic to edit notes
-        for (let index = 0; index < notes.length; index++) {
-          const element = notes[index];
+        for (let index = 0; index < newNotes.length; index++) {
+          const element = newNotes[index];
           if(element._id===id){
-            element.title=title;
-            element.description=description;
-            element.tag=tag;
+            newNotes[index].title = title;
+            newNotes[index].description = description;
+            newNotes[index].tag = tag; 
+            break; 
           }
           
         }
-
+          setNotes(newNotes);
       }
     return (
         <NoteContext.Provider value={{notes,addNote,deleteNote,editNote,getNotes}}>
